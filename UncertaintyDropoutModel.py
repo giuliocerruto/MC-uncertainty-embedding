cedcbb325d8433088515ee80f7baba90e5e3ad66
giuliocerruto <68821738+giuliocerruto@users.noreply.github.com>
2021-05-26 21:00:16 +0200

    Update UncertaintyDropoutModel.py

--- UncertaintyDropoutModel.py
+++ UncertaintyDropoutModel.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 from inspect import signature
 from functools import partial
 from .SampleWeightScheduler import SampleWeightScheduler
-#from SampleWeightScheduler import SampleWeightScheduler
+from tensorflow.python.keras.engine import compile_utils
 
 
 class UncertaintyDropoutModel(keras.Model):
