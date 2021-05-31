a3aaf64efff1cc645adf746781467770c30416e9
giuliocerruto <68821738+giuliocerruto@users.noreply.github.com>
2021-05-31 23:19:02 +0200

    Update SampleWeightScheduler.py

--- SampleWeightScheduler.py
+++ SampleWeightScheduler.py
@@ -3,12 +3,12 @@ from keras.callbacks import Callback
 
 
 class SampleWeightScheduler(Callback):
-
-    def __init__(self, schedule, verbose=0):
+      
+      def __init__(self, schedule, verbose=0):
         super(SampleWeightScheduler, self).__init__()
-        self.schedule = schedule
-        self.verbose = verbose
-
-    def on_epoch_begin(self, epoch, logs=None):
+        self.schedule= schedule
+        self.verbose= verbose
+      
+      def on_epoch_begin(self, epoch, logs=None):
         fun = self.model.uncertainty_function
         self.model.epoch_uncertainty_function = self.schedule(epoch, fun)
